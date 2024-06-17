6c9efa9dda05c6250f59f9fdca7e628d529b5908
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-17 22:14:12 +0300

    Добавлены новые url в приложении users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import HomePageView, RegisterView
 
 app_name = UsersConfig.name
 urlpatterns = [
-
+    path('login/', LoginView.as_view(template_name='users/user_login.html'), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
