86bcd0bff04c006a1fb6f922ba342a9813032b34
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-19 19:23:29 +0300

    Добавлены новые URL

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import HomePageView, RegisterView
+from users.models import User
+from users.views import HomePageView, RegisterView, OTPConfirmView, UserDetailView
 
 app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/user_login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('confirm/', OTPConfirmView.as_view(), name='otp_confirm'),
+    path('profile/<int:pk>/', UserDetailView.as_view(), name='detail')
 ]
