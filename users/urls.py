2d006737de2f58546bcd188b16830b4636e8be2b
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-19 21:22:48 +0300

    Изменены контроллеры в настройках URL

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.models import User
-from users.views import HomePageView, RegisterView, OTPConfirmView, UserDetailView
+from users.views import RegisterView, OTPConfirmView, UserDetailView, MyLoginView
 
 app_name = UsersConfig.name
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/user_login.html'), name='login'),
+    path('login/', MyLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('confirm/', OTPConfirmView.as_view(), name='otp_confirm'),
