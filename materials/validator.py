8941f1b99e662fc42cbf8da5d093dd61811f09c7
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-20 20:03:06 +0300

    Создан валидатор URL (принимаются только ссылки на youtube и его производные)

---
+++ materials/validator.py
@@ -0,0 +1,19 @@
+import re
+
+from rest_framework import serializers
+
+
+class URLValidator:
+
+    def __init__(self, url):
+        self.url = url
+
+    def __call__(self, value):
+        reg = re.compile(r'^(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+$',)
+        tmp_url = dict(value).get(self.url)
+        if not bool(reg.match(tmp_url)):
+            print(False)
+            print(tmp_url)
+            raise serializers.ValidationError('Данная ссылка не является ссылкой на Youtube')
+        else:
+            print(True)
