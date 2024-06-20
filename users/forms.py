016a9cacd7684eba088b16711ea7ae75ebef68d7
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-20 19:56:09 +0300

    Переопределены валидаторы регистрации

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,30 @@
+import re
+
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 from users.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    error_messages = {
+        'password_mismatch': "Пароли не совпадают",
+    }
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Пользователь с такой почтой уже существует")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        if len(password1) < 8:
+            raise forms.ValidationError("Пароль слишком короткий. Минимум 8 символов")
+
