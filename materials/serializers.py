ab2c962ff7268f1a68fd9d9fbc3f1a3ccf233f24
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-20 19:57:15 +0300

    Добавлен валидатор URL

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from materials.models import EducationModule, Lesson, Subscription
+from materials.validator import URLValidator
 
 
 class MaterialSerializer(serializers.ModelSerializer):
@@ -13,9 +14,11 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [URLValidator(url='URL')]
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = '__all__'
+
