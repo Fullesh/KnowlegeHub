1bb7a1dd14b294e60aceaec29357a9e31c37384c
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-21 18:47:10 +0300

    Убраны debug строки в валидаторе URL

--- materials/validator.py
+++ materials/validator.py
@@ -12,8 +12,4 @@ class URLValidator:
         reg = re.compile(r'^(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+$',)
         tmp_url = dict(value).get(self.url)
         if not bool(reg.match(tmp_url)):
-            print(False)
-            print(tmp_url)
             raise serializers.ValidationError('Данная ссылка не является ссылкой на Youtube')
-        else:
-            print(True)
