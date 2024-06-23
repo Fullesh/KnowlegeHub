2d63281e86e837e82f01bc6c069ea7f7832c9ae1
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-23 13:26:41 +0300

    Добавлен URL для восстановления пароля

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, OTPConfirmView, UserDetailView, MyLoginView, UserDeleteView
+from users.views import RegisterView, OTPConfirmView, UserDetailView, MyLoginView, UserDeleteView, res_password
 
 app_name = UsersConfig.name
 urlpatterns = [
@@ -11,5 +11,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('confirm/', OTPConfirmView.as_view(), name='otp_confirm'),
     path('profile/<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('delete/<int:pk>/', UserDeleteView.as_view(), name='delete')
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='delete'),
+    path('reset_password/', res_password, name='reset_password')
 ]
